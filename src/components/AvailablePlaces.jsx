92f7192cf786f05b277f36c558f270347b011dab
richard <richardsunday0812@gmail.com>
2026-03-03 14:26:33 +0100

    refactor: improve fetching logic in AvailablePlaces component and update loading state handling

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(true);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [error, setError] = useState(null);
 
@@ -12,17 +14,28 @@ export default function AvailablePlaces({ onSelectPlace }) {
       try {
         const response = await fetch("http://localhost:3000/places");
         const data = await response.json();
+        setIsFetching(true);
 
         if (!response.ok) {
-          const error = new Error("Failed to fetch places data.");
           throw new Error("Failed to fetch places data.");
         }
-        setAvailablePlaces(data.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlace = sortPlacesByDistance(
+            data.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlace);
+          setIsFetching(false);
+        });
+
       } catch (error) {
         setError({
           message:
             error.message || "An error occurred while fetching places data.",
         });
+        setIsFetching(false);
       }
     }
     fetchPlaces();
@@ -36,7 +49,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
-      isLoading={availablePlaces.length === 0}
+      isLoading={isFetching}
       loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
